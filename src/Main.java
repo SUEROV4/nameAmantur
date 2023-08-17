544f9c473dbb9f0f6d53970692de2879f1514b0c
User <amantursuerov166@gmail.com>
2023-08-17 15:26:40 +0600

    amantur

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    public static int amantur(){
+    public static int jamantur(){
         return 2;
     }
 }
