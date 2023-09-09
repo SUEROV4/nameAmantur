9d6305c245476d9e1ed3c897d512cb0bf313d814
User <amantursuerov166@gmail.com>
2023-09-09 19:07:23 +0600

    Amantur

---
+++ src/Popugay.java
@@ -0,0 +1,50 @@
+public class Popugay{
+  private String  kanat;
+  private String tumshuk;
+  private String svet;
+
+  public String getKanat() {
+    return kanat;
+  }
+
+  public void setKanat(String kanat) {
+    this.kanat = kanat;
+  }
+
+  public String getTumshuk() {
+    return tumshuk;
+  }
+
+  public void setTumshuk(String tumshuk) {
+    this.tumshuk = tumshuk;
+  }
+
+  public String getSvet() {
+    return svet;
+  }
+
+  public void setSvet(String svet) {
+    this.svet = svet;
+  }
+
+  public Popugay(String kanat, String tumshuk, String svet) {
+    this.kanat = kanat;
+    this.tumshuk = tumshuk;
+    this.svet = svet;
+  }
+
+  @Override
+  public String toString() {
+    return "Popugay{" +
+            "kanat='" + kanat + '\'' +
+            ", tumshuk='" + tumshuk + '\'' +
+            ", svet='" + svet + '\'' +
+            '}';
+  }
+
+  public Popugay() {
+
+  }
+}
+
+
