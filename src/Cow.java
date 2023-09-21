35a2582a96d3bd005608ad81459d638a59793aad
User <amantursuerov166@gmail.com>
2023-09-21 18:32:26 +0600

    amantur

---
+++ src/Cow.java
@@ -0,0 +1,58 @@
+public class Cow {
+    private String weight;
+    private String age;
+    private String gender;
+    private String pincName;
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPincName() {
+        return pincName;
+    }
+
+    public void setPincName(String pincName) {
+        this.pincName = pincName;
+    }
+
+    public Cow(String weight, String age, String gender, String pincName) {
+        this.weight = weight;
+        this.age = age;
+        this.gender = gender;
+        this.pincName = pincName;
+    }
+
+    public Cow() {
+    }
+
+    @Override
+    public String toString() {
+        return "Cow{" +
+                "weight='" + weight + '\'' +
+                ", age='" + age + '\'' +
+                ", gender='" + gender + '\'' +
+                ", pincName='" + pincName + '\'' +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,42 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String a = scanner.nextLine();
-        switch (a) {
-            case "NAARYN":
-                System.out.println(Region.NAARYN);
-                break;
-            case "CNUY":
-                System.out.println(Region.CHUY);
-                break;
-            case "batken":
-                System.out.println(Region.batken);
-                break;
-            case "OSH":
-                System.out.println(Region.OSH);
-                break;
-            case "TALAS":
-                System.out.println(Region.TALAS);
-                break;
-            case "djalal_Abad":
-                System.out.println(Region.djalal_Abad);
-        }
+Cow cow =new Cow();
+cow.setAge("12");
+cow.setGender("man");
+cow.setPincName("SuerovAmantur");
+cow.setWeight("14");
+        System.out.println(cow);
+
+
+
+        Sheep sheep =new Sheep();
+        sheep.setAge("15");
+        sheep.setGender("myskoi");
+        sheep.setPincName("SuerovAmantur");
+        sheep.setWeight("58");
+        System.out.println(sheep);
+
+
+
+        Horse horse =new Horse();
+        horse.setAge("13");
+        horse.setColor("krasnuy");
+        horse.setPincName("SuerovAmantur");
+        horse.setWeight("57");
+        horse.setGender("myskoy");
+            System.out.println(horse);
+
+
+
+            Farm farm =new Farm();
+            farm.setCows("korova");
+            farm.setAdsres("46.1");
+            farm.setHorses("loshad");
+            farm.setOwnerName("Daniel");
+            farm.setSheep("baran");
+        System.out.println(farm);
     }
 }
