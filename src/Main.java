a639dc8b29b886522d7159855bcab3409b039ea0
User <amantursuerov166@gmail.com>
2023-09-24 13:16:45 +0600

    ssd

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,20 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-Cow cow =new Cow();
-cow.setAge("12");
-cow.setGender("man");
-cow.setPincName("SuerovAmantur");
-cow.setWeight("14");
-        System.out.println(cow);
+        Scanner scanner = new Scanner(System.in);
 
-
-
-        Sheep sheep =new Sheep();
-        sheep.setAge("15");
-        sheep.setGender("myskoi");
-        sheep.setPincName("SuerovAmantur");
-        sheep.setWeight("58");
-        System.out.println(sheep);
-
-
-
-        Horse horse =new Horse();
-        horse.setAge("13");
-        horse.setColor("krasnuy");
-        horse.setPincName("SuerovAmantur");
-        horse.setWeight("57");
-        horse.setGender("myskoy");
-            System.out.println(horse);
-
-
-
-            Farm farm =new Farm();
-            farm.setCows("korova");
-            farm.setAdsres("46.1");
-            farm.setHorses("loshad");
-            farm.setOwnerName("Daniel");
-            farm.setSheep("baran");
-        System.out.println(farm);
+        try {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            if (a > b) {
+                System.out.println("бизди б кичине ");
+            } else {
+                System.out.println("бизде а кичине");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Amantur");
+        }
     }
 }
