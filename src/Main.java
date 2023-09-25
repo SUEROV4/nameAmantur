6a1561730435ffdbb2329994b4dbcaaf6b43dbc3
User <amantursuerov166@gmail.com>
2023-09-25 14:34:14 +0600

    amantur

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Dom[] array = new Dom[4];
+        array[0] = new Dom("Amantur",16,"Suerov");
+        array[1] = new Dom("Bael",23,"Suerov");
+        array[2] = new Dom("Gulshaiyr",42,"Arapova");
+        array[3] = new Dom("Aktilek",20,"Suerov");
+        System.out.println(Arrays.toString(array));
+        System.out.println("--------------------");
+        Dormitory[] array1 = new  Dormitory[4];
+        array1[0] = new Dormitory("jhrvkhbjrv",23,"fghj");
+        array1[1] = new Dormitory("dfghj",15,"dfghj");
+        array1[2] = new Dormitory("fghjk",21,"dfghj");
+        array1[3] = new Dormitory("fghjk",21,"dfgh");
+        System.out.println(Arrays.toString(array1));
+        System.out.println("---------------------");
+        Hotel[] array2 = new Hotel[4];
+        array2[0] = new Hotel("fghj",11,"dfghj");
+        array2[1] = new Hotel("fghj",12,"dfghj");
+        array2[2] = new Hotel("fghj",13,"dfghj");
+        array2[3] = new Hotel("fghj",14,"dfghj");
+        System.out.println(Arrays.toString(array2));
 
-        try {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            if (a > b) {
-                System.out.println("бизди б кичине ");
-            } else {
-                System.out.println("бизде а кичине");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Amantur");
-        }
     }
 }
